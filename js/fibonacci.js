e8797b8b33957a3c2d4a6269d5ea911b9bd5c008
Kyle Gunter <kylewgunter@gmail.com>
2021-05-25 16:06:57 -0500

    Sarah and Kyle Fibonacci

--- js/fibonacci.js
+++ js/fibonacci.js
@@ -1,5 +1,19 @@
 const fibonacci = (num) => {
-    
+  if (num === 0) {
+    return 0;
+  } else if (num === 1) {
+    return 1;
+  }
+  
+  let num1 = 0, num2 = 1
+  
+  for(let i = 2; i <= num; i++){
+    let temp = num1 + num2
+    num1 = num2
+    num2 = temp
+  }
+  return num2
 }
+
 
 module.exports = {fibonacci}
