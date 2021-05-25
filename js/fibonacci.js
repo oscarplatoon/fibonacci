3ef07e5a548b498a7813a509bb105d588eb87da1
stesun2 <ss27@illinois.edu>
2021-05-25 14:34:32 -0500

    Stephen Sun & Shawn Kiernan completed fibbonacci

--- js/fibonacci.js
+++ js/fibonacci.js
@@ -1,5 +1,18 @@
 const fibonacci = (num) => {
-    
+
+  // base cases 0 or 1
+  if (num === 0) {
+    return 0;
+  }
+  if (num === 1) {
+    return 1;
+  }
+  // recursive call F(n-1) + F(n-2)
+  return fibonacci(num - 1) + fibonacci(num - 2)
+      
 }
 
 module.exports = {fibonacci}
+
+
+// 0, 1, 1, 2, 3, 5, 8, 13, 21
