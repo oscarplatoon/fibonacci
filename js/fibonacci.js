be1992129873ac6f68ad909b88a7352320407b43
JWShuff <16825267+JWShuff@users.noreply.github.com>
2021-05-25 15:10:59 -0400

    Jung Park and Jack Shuff Fibonacci solution

--- js/fibonacci.js
+++ js/fibonacci.js
@@ -1,5 +1,16 @@
 const fibonacci = (num) => {
-    
+  let fibonacci = [0, 1];
+  inputIndex = num;
+
+  //Catch if input wants the 0 term of the Fibonacci Sequence)
+  if (inputIndex == 0) return 0
+  //Generates an array with fibonacci sequence numbers up to the requested index
+  for (let i = 2; i <= inputIndex; i++) {
+    fibonacci.push(fibonacci[i-2] + fibonacci[i-1])
+  }
+
+  //pops the requested index from the end of the generated array.
+  return fibonacci.pop()
 }
 
 module.exports = {fibonacci}
