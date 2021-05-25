b14149f171c3e74c93708465de33ac7245fb010b
Smattonline <smattonline@gmail.com>
2021-05-25 15:38:59 -0700

    Musa and Jonas' Fibonnaci Solution

--- js/fibonacci.js
+++ js/fibonacci.js
@@ -1,5 +1,17 @@
 const fibonacci = (num) => {
+	let sequence = [0, 1]
+	let answer = 0
+	for (var i = 2; i < num; i++) {
+		sequence.push(sequence[i-1] + sequence[i-2])
+		if (num == 0) {
+		console.log(0)
+		} else if (num = 1) {
+		console.log(1)
+		}
+
+	}
+	console.log(sequence)
     
 }
 
-module.exports = {fibonacci}
+console.log(fibonacci(5))// === 5)
