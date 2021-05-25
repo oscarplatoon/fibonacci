ee2246ce9a65f5fe97e3416e9a9e9e35f55d80e4
Marc Stanley <marcson909@gmail.com>
2021-05-25 16:26:51 -0500

    fibonacci function starting at Fn(0) and using recursion to solve

--- js/fibonacci.js
+++ js/fibonacci.js
@@ -1,5 +1,14 @@
 const fibonacci = (num) => {
+    if (num === 1){
+    return 1;
+  } else if ( num === 0) {
+    return 0;
+  } else {
+    return fibonacci(num - 1) + fibonacci(num - 2);
+  }
     
-}
+};
 
-module.exports = {fibonacci}
+console.log(fibonacci(4));
+
+//module.exports = {fibonacci}
