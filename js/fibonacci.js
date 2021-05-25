052d8b1afcc2181ed5ea7ba79eb6bb234faf3205
RenoAverill <reno_averill@yahoo.com>
2021-05-25 12:49:28 -0600

    John and Reno's solution fibonacci

--- js/fibonacci.js
+++ js/fibonacci.js
@@ -1,5 +1,10 @@
 const fibonacci = (num) => {
+    if (num === 1) return 1
+
+    if (num <= 1) return num
     
+    else return fibonacci(num - 1) + fibonacci(num - 2)
 }
 
 module.exports = {fibonacci}
+console.log(fibonacci(7))
