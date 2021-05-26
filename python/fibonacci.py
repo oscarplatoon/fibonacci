8f3adff64eb93760dd2be636861c91f93ddcf05b
Tim Gaston <Gman2105@users.noreply.github.com>
2021-05-26 20:24:09 +0000

    Eric and Tim fibonacci solution

--- python/fibonacci.py
+++ python/fibonacci.py
@@ -1,4 +1,16 @@
 def fibonacci(n):
-  pass
+    
+    if n < 0:
+        print("Incorrect input")
+        
+    elif n == 0:
+        return 0
+        
+    elif n == 1 or n == 2:
+        return 1
+
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+  
 
 
