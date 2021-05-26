e502c6e6e078112416c44d3d36e31b3ce9d9bf5f
vzbrunstad <vzbrunstad@msn.com>
2021-05-26 16:49:52 -0500

    Vince B fibonacci js and py Complete

--- python/fibonacci.py
+++ python/fibonacci.py
@@ -1,4 +1,17 @@
 def fibonacci(n):
-  pass
+
+  my_array = [0,1]
+  x=1
+  while x < n:
+    
+    my_array.append(my_array[x]+ my_array[x-1])
+    
+    x=x+1
+
+  return my_array[n]
+  
+fibonacci(8)
+
+#  pass
 
 
