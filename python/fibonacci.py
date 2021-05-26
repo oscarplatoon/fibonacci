2a63e7752ba1c2a5ace069d7916b975eea74732f
jakewhite5 <jakewhite5@yahoo.com>
2021-05-26 09:49:46 -0700

    completed in python

--- python/fibonacci.py
+++ python/fibonacci.py
@@ -1,4 +1,15 @@
-def fibonacci(n):
-  pass
+def fibonacci(num):
+  past = 0
+  current = 1
+  if num <= 1:
+    return num
+  for num in range(num - 1):
+    next = current + past
+    past = current
+    current = next
+  return current
+
+fibonacci(8)
+
 
 
